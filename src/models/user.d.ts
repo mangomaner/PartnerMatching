273fc3bbc8fd68eb78c514c87e98589d2f12547a
mangomaner <2756477287@qq,com>
2024-09-19 22:11:07 +0800

    t

---
+++ src/models/user.d.ts
@@ -0,0 +1,14 @@
+export type UserType={
+    id: number;
+    username: string,
+    userAccount: string;
+    avatarUrl?: string;
+    gender: number;
+    phone: string;
+    email: string;
+    userStatus: number;
+    userRole: number;
+    createTime: Date;
+    userCode: string;
+    tags: string[];
+};
--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
-
+const user = {
+  id: 1,
+  username: '盲果人',
+  userAccount: 'mangoman',
+  avatarUrl: 'https://i2.hdslb.com/bfs/face/10025ca67d25c6ea9a33c9ae163ad426544d5dcf.jpg@120w_120h_1c',
+  gender: 1,
+  phone: '177397245',
+  email: '114514@qq.com',
+  userRole: 1,
+  createTime: new Date(),
+  userCode: '1145',
+  tags: ['男','大三'],
+}
 </script>
 
 <template>
